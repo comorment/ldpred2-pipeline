581613c6c221ecd271750d517f6e9e7290b8b3ea
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2023-03-23 12:09:37 +0000

    adding tests etc.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ldpred2-pipeline
 LDpred2 Docker setup for FinnGen
+
+## building
+
+    docker build -t ldpred2 -f docker/Dockerfile .
+
+
+## run
+
+Interactive R:
+
+    docker run -it ldpred2 R
+
+Invoking Rscript:
+
+    docker run ldpred2 Rscript <filename>.R
+
+
+## test
+
+    py.test -v test/test_ldpred2.py
+
+
+Note: this will only work in environments where Docker does not require sudo.
---
+++ tests/test_ldpred2.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+"""
+Test module for ``r.sif`` build
+"""
+
+import os
+import subprocess
+
+
+def test_R():
+    call = 'docker run ldpred2 R --version'
+    out = subprocess.run(call.split(' '))
+    assert out.returncode == 0
+
+
+def test_Rscript():
+    call = 'docker run ldpred2 Rscript --version'
+    out = subprocess.run(call.split(' '))
+    assert out.returncode == 0
+
+
+def test_R_packages():
+    pwd = os.getcwd()
+    call = f'docker run -i -t --mount type=bind,source=$PWD,target=/home ldpred2 Rscript /home/tests/import_libraries.R'
+    out = subprocess.run(call.split(' '))
+    assert out.returncode == 0
